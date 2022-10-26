79a96c297043337ef213c17a122063e350904595
jd8890 <104278313+jd8890@users.noreply.github.com>
2022-10-26 14:00:01 +0530

    Create enqueue.c

---
+++ enqueue.c
@@ -0,0 +1,11 @@
+void enQueue(int value) {
+  if (rear == SIZE - 1)
+    printf("\nQueue is Full!!");
+  else {
+    if (front == -1)
+      front = 0;
+    rear++;
+    items[rear] = value;
+    printf("\nInserted -> %d", value);
+  }
+}
